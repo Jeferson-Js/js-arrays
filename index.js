ccdd00a69acf0f67c0608db07b448cd3ef463d5f
Your Name3 <you@example.com>
2022-12-29 16:59:35 -0300

    Trabalhando com strutura de dados em js

---
+++ index.js
@@ -0,0 +1,11 @@
+//const media = [10,6.5,8,7.5];
+//console.log(media)
+
+const nota1 = 10;
+const nota2 = 6.5;
+const nota3 = 8;
+const nota4 = 7.5;
+
+const media = (nota1 + nota2 + nota3 + nota4) / 4; 
+
+console.log(media);
