19df7ae0e4c31ca4adf23f071b0c6463111b860a
Your Name3 <you@example.com>
2022-12-29 18:26:43 -0300

    Trabalhando com dimensões entre arrays

---
+++ dimensoes.js
@@ -0,0 +1,8 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias]
+
+console.log(`A aluna da posição 1 é ${listaDeAlunosEMedias[0][2]}`)
+console.log(`A aluna da posição 1 é ${listaDeAlunosEMedias[1][1]}`)
+ 
